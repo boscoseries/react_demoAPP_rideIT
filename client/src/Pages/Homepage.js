7cf0dfd0a8ab04cb5fe32e4b07b1fe93e7c168e8
Johnbosco <boscoseries@gmail.com>
2019-06-30 13:53:15 +0100

    added home component

---
+++ client/src/Pages/Homepage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+      <header className="masthead">
+        <div className="container h-100">
+          <div className="row h-100 align-items-center justify-content-center text-center">
+            <div className="col-lg-10 align-self-end">
+              <h1 className="text-uppercase text-white font-weight-bold">
+                Let's Get you on your way
+              </h1>
+              <hr className="divider my-4" />
+              <div className="col-lg-8 align-self-baseline">
+                <p className="text-white-75 font-weight-light mb-5">
+                  Set out to that event without delay. RideIT provides
+                  quick and easy access to automobile services.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+  );
+}
